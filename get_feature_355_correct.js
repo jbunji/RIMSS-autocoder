2dd07f2decff2270a38e7744aabb743f4804b8fa
Justin Bundrick <justinbundrick@gmail.com>
2026-01-20 11:13:41 -0500

    Clean up temporary feature query scripts from Feature #355 session

--- get_feature_355_correct.js
+++
@@ -1,4 +0,0 @@
-const db = require('better-sqlite3')('features.db');
-const feature = db.prepare('SELECT * FROM features WHERE id = ?').get(355);
-console.log(JSON.stringify(feature, null, 2));
-db.close();
