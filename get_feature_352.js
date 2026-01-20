edafbb8f7bd349698ec1d65624e1cb998b7d93a1
Justin Bundrick <justinbundrick@gmail.com>
2026-01-20 11:07:56 -0500

    Clean up temporary test files for Feature #352

--- get_feature_352.js
+++
@@ -1,5 +0,0 @@
-const Database = require('better-sqlite3');
-const db = new Database('assistant.db');
-const feature = db.prepare('SELECT * FROM features WHERE id = ?').get(352);
-console.log(JSON.stringify(feature, null, 2));
-db.close();
